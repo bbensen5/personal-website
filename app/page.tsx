b3ad47bb953f5ad567ff18c4271424d604f5563a
Bianca Bensen <bianca06162007@gmail.com>
2026-07-02 12:56:47 -0700

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
       className="flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black"
       style={{ fontFamily: '"Times New Roman", Times, serif' }}
     >
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-0 px-0 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src="/slackpfp.JPG"
+          alt="Bianca's face"
+          width={400}
+          height={400}
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
@@ -25,6 +25,8 @@ export default function Home() {
             <a
               href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
               className="font-medium text-zinc-950 dark:text-zinc-50"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Mystery
             </a>{" "}
@@ -32,6 +34,8 @@ export default function Home() {
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Learning
             </a>{" "}
@@ -41,18 +45,18 @@ export default function Home() {
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/bbensen5"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/github-logo.svg"
               alt="Vercel logomark"
               width={16}
               height={16}
             />
-            Deploy Now
+            My GitHub
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
